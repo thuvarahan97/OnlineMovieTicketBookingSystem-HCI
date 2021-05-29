1135e79e711df582ca5a5eec6dbff69d23cc52d3
dilanirajen <47143245+dilanirajen@users.noreply.github.com>
2021-05-29 08:29:27 +0530

    Initial Commit-Movie Alignment

--- movies_events.php
+++ movies_events.php
@@ -3,37 +3,46 @@
 <div class="content">
 	<div class="wrap">
 		<div class="content-top">
-			<center><h1 style="color:#555;">(NOW SHOWING)</h1></center>
+			<h1 style="color:#555;">NOW SHOWING</h1>
 			
 			<?php
-          	 $today=date("Y-m-d");
-          	 $qry2=mysqli_query($con,"select * from  tbl_movie ");
-		
-          	  while($m=mysqli_fetch_array($qry2))
-                   {
-                    ?>
+				$numOfCols = 4;
+				$rowCount = 0;
+				$bootstrapColWidth = 12 / $numOfCols;
+				$today=date("Y-m-d");
+				$qry2=mysqli_query($con,"select * from  tbl_movie ");
+			
+				while($m=mysqli_fetch_array($qry2))
+					{
+					if($rowCount % $numOfCols == 0) { ?> <div class="row"> <?php } 
+						$rowCount++;
+			?>
                     
-                    <div class="col_1_of_4 span_1_of_4">
-					<div class="imageRow">
-						  	<div class="single">
-						  		<?php
-						
-						?>
-						  		<a href="about.php?id=<?php echo $m['movie_id'];?>"><img src="<?php echo $m['image'];?>" alt="" /></a>
-						  	</div>
-						  	<div class="movie-text">
-						  		<h4 class="h-text"><a href="about.php?id=<?php echo $m['movie_id'];?>" style="text-decoration:none;"><?php echo $m['movie_name'];?></a></h4>
-						  		Cast: <Span class="color2" style="text-decoration:none;"><?php echo $m['cast'];?></span><br>
+				<div class="col-md-<?php echo $bootstrapColWidth; ?>" >
+					<div class="thumbnail" >
+						<div class="single">
+						 
+						  	<a href="about.php?id=<?php echo $m['movie_id'];?>">
+							  <img style="width:100%" src="<?php echo $m['image'];?>"  />
+							</a>
+						</div>
+						<div class="movie-text">
+						  	<center>
+								 <h4 class="h-text"><a href="about.php?id=<?php echo $m['movie_id'];?>" style="text-decoration:none;"><?php echo $m['movie_name'];?></a></h4>
+							</center>
+						  	Cast: 
+							<Span class="color2" style="text-decoration:none;size: 30px;fontSize:30px;"><?php echo $m['cast'];?></span><br>
 						  		
-						  	</div>
-		  				</div>
+						</div>
+		  			</div>
 		  		</div>
 		  		
-  	    <?php
-  	    	}
+  	    	<?php
+  	    	if($rowCount % $numOfCols == 0) { ?> </div> <?php } } 
   	    	?>
 			
 			</div>
 				<div class="clear"></div>		
 			</div>
-			<?php include('footer.php');?>
+		</div>
+	<?php include('footer.php');?>
