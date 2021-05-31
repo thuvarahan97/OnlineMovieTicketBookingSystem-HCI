488e6f51e6b333e3c044d47ca9a987fab85c5dca
Thuvarahan <thuvarahan97@gmail.com>
2021-05-31 15:19:34 +0530

    Update script_thuva_multi_carousel.js

--- js/script_thuva_multi_carousel.js
+++ js/script_thuva_multi_carousel.js
@@ -5,7 +5,7 @@ $('#movieCarousel_nowshowing[data-type="multi"] .item').each(function() {
 	}
 	next.children(':first-child').clone().appendTo($(this));
 
-	for (var i = 0; i < 3; i++) {
+	for (var i = 0; i < 4; i++) {
 		next = next.next();
 		if (!next.length) {
 			next = $(this).siblings(':first');
@@ -23,7 +23,7 @@ $('#movieCarousel_upcoming[data-type="multi"] .item').each(function() {
 	}
 	next.children(':first-child').clone().appendTo($(this));
 
-	for (var i = 0; i < 3; i++) {
+	for (var i = 0; i < 4; i++) {
 		next = next.next();
 		if (!next.length) {
 			next = $(this).siblings(':first');
