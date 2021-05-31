4834cbaf99b44df78c01645dae1014207be3609e
Thuvarahan <thuvarahan97@gmail.com>
2021-05-31 18:45:33 +0530

    home redirection fix

--- theatre/pages/index.php
+++ theatre/pages/index.php
@@ -11,7 +11,7 @@ include('header.php');
         Theatre Assistance
       </h1>
       <ol class="breadcrumb">
-        <li><a href="index"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li><a href="index.php"><i class="fa fa-dashboard"></i> Home</a></li>
         <li class="active">Home</li>
       </ol>
     </section>
